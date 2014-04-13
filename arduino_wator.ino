aaa9b9a00b32d1074256fe02770c9ec77a8dbd1e
7flying <diez.f.irene@gmail.com>
2014-04-13 22:06:17 +0200

    coordinates for moving + fish's behaviour

--- arduino_wator.ino
+++ arduino_wator.ino
@@ -15,18 +15,19 @@ typedef struct {
 } Element;
 
 Adafruit_BicolorMatrix matrix = Adafruit_BicolorMatrix();
-Element board [][8] = { { {'f',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0} },
-                        { {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0} },
-                        { {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0} },
-                        { {'w',0,0}, {'w',0,0}, {'f',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0} },
+Element board [][8] = { { {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0} },
                         { {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0} },
                         { {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0} },
                         { {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0} },
-                        { {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'f',0,0} }
+                        { {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0} },
+                        { {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0} },
+                        { {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0} },
+                        { {'f',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0}, {'w',0,0} }
                       };
 void setup() {
  matrix.begin(0x70);
  randomSeed(analogRead(0));
+ Serial.begin(9600);
 }
 /* Paints the board  */
 void paint() {
@@ -34,10 +35,10 @@ void paint() {
  for(uint8_t i = 0; i < 8; i++) {
   for(uint8_t j = 0; j < 8; j++) {
     if(board[i][j].type == 's')
-      matrix.drawPixel(i,j,LED_RED);
+      matrix.drawPixel(j,i,LED_RED);
      else
        if(board[i][j].type == 'f')
-         matrix.drawPixel(i,j, LED_YELLOW);
+         matrix.drawPixel(j,i, LED_YELLOW);
   } 
  }
  matrix.writeDisplay();
@@ -74,18 +75,80 @@ void nextChronon() {
   }
 }
 /* Reproduces the creature at (x, y) */
-void reproduce(uint8_t x, uint8_t y) {
-  board[x][y].survivedChro = 0;
-  char type = board[x][y].type;
-  if(moveCreature(x,y)) { // The creature succesfully moved
-    board[x][y].type = type;
+void reproduce(uint8_t i, uint8_t j) {
+  board[i][j].survivedChro = 0;
+  char type = board[i][j].type;
+  if(moveCreature(i,j)) { // The creature succesfully moved
+    board[i][j].type = type;
     if(type == 's')
-      board[x][y].energy = SE;
+      board[i][j].energy = SE;
   }
 }
 /* Moves randomly the creature at (i, j) */
 boolean moveCreature(uint8_t i, uint8_t j) {
-  return false; //TODO
+ Serial.println();
+ Serial.print("Want to move creature at  row:");
+ Serial.print(i);
+ Serial.print(" column: ");
+ Serial.print(j);
+ Serial.println(")");
+ if(board[i][j].type != 'w') {
+   int r = i, c = j;
+   // Plan of moves
+   // |-|1|-|
+   // |2|x|3|
+   // |-|4|-|
+   // Positions      1      2      3      4
+   uint8_t pos = { {0,0}, {0,0}, {0,0}, {0,0} }
+   pos[0][1] = c;  // for 1 column does not change
+   pos[1][0] = r;  // for 2 row does not change
+   pos[2][0] = r;  // for 3 row does not change
+   pos[3][1] = c;  // for 4 column does not change
+   // We are in a torus
+   if(r == 0)
+     pos[0][0] = 7; // For 1 go to last row
+   else
+     if(r == 7)
+       pos[3][0] = 0; // For 4 go to first row
+     else {
+       // Normal moves
+       pos[0][0] = r - 1;
+       pos[3][0] = r + 1; 
+     }
+   if(c == 0)
+     pos[1][1] = 7; // For 2 go to last column
+   else
+     if(c == 7)
+       pos[2][1] = 0; // For 3 go to first column
+     else {
+       // Normal moves
+       pos[1][1] = c - 1;
+       pos[2][1] = c + 1; 
+     }
+   // Behaviours
+   int choose = -1;
+   // Check space and food
+   boolean space = false, fish = false;
+   for(uint8_t it = 0 ; it < 4 ; it++){
+     if(board[pos[it][0]][pos[it][1]].type == 'w')
+       space = true;
+     else
+       if(board[pos[it][0]][pos[it][1]].type == 'f')
+         fish = true;
+   }
+   boolean ret = false;
+   int choose = -1;
+   if(board[i][j].type == 'f' && space) {
+     ret = true; // we can move
+     choose = random(4);
+     while(board[pos[choose][0]][pos[choose][1]].type != 'w'){
+       choose = random(4);
+     }
+   }
+   // TODO Shark's behaviour
+   // TODO update movements
+   return ret;
+ }else return false;
 }
 void loop() {
  paint();
